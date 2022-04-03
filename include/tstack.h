ef846e6fe745a8f068c485b4455e28d8433406d4
NikitaAks <99181243+NikitaAks@users.noreply.github.com>
2022-04-03 23:20:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,26 +1,28 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
-private:
-   T arr[100];
-   int top;
-public:
-   TStack(): top(-1) { }
-   T get() const {
-       return arr[top];
-   }
-   bool isEmpty() const {
-       return top == - 1;
-   }
-   void pop() {
-       if (top >= 0)
-           top--;
-   }
-   void push(T item) {
-       if (top < size - 1)
-           arr[++top] = item;
-   }
+class TStack {
+ private:
+  T arr[50];
+  int top;
+
+ public:
+  TStack():top(-1) {}
+  bool isFull() const {
+    return size == size - 1;
+  }
+  bool isEmpty() const {
+  return top == -1;
+  }
+  T get() {
+    return arr[top];
+  }
+  void pop() {
+    if (top >= 0)
+      top--;
+  }
+  void push(T value) {
+    if (top < size - 1)
+      arr[++top] = value;
+  }
 };
 #endif  // INCLUDE_TSTACK_H_
